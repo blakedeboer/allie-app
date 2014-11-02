94a72752b7784286826d70016fbcfcb91154b55b
blakedeboer <deboer.blake@gmail.com>
2014-11-02 09:35:20 -0500

    scraper done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ActiveRecord::Base
 
+  PAGE = "http://ruby006.students.flatironschool.com/"
 
 end
--- lib/assets/scraper.rb
+++ lib/assets/scraper.rb
@@ -1,32 +1,57 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 require_relative '../../config/environment'
 
 
-PAGE = "http://ruby006.students.flatironschool.com/"
+class Scraper
+  attr_accessor :student_htmls, :full_page_html
 
-html = Nokogiri.HTML(open(PAGE))
+  def initialize
+  end
 
-student_html = html.css("li.home-blog-post")
+  def scrape_page #=> full page of html
+    Nokogiri.HTML(open(Student::PAGE))
+  end
 
-img = student_html.css("div.blog-thumb a img")[0]['src']
+  def parse_students #=> select html for all students
+    full_page_html = self.scrape_page
+    full_page_html.css("li.home-blog-post")
+  end
 
-name = student_html.css("div.big-comment h3 a")[0].text
+  def parse_student_name(html) #=> "name"
+    html.css("div.big-comment h3 a").text
+  end
 
-@student = Student.new
-@student.name = name
-@student.image = img
-@student.save
+  def parse_student_image(html) #=> "image path"
+    html.css("div.blog-thumb a img").map {|img| img["src"]}[0]
+  end
 
-puts "#{@student.id}"
-puts "#{@student.name}"
-puts "#{@student.image}"
+  def create_students #=> saves each student in DB w/ name and image path
+    student_htmls = parse_students
+    student_htmls.each do |student_html|
+      if parse_student_name(student_html) != "Joe Lehr" && parse_student_name(student_html) != "Iris Abramson"
+        @student = Student.new
+        @student.name = parse_student_name(student_html)
+        @student.image = parse_student_image(student_html)
+        @student.save
+      end
+    end
+  end
 
+end
 
-# src  = html.at('.profilePic img')['src']
+Scraper.new.create_students
 
-
-# File.open("foo.png", "wb") do |f|
-#   f.write(open(src).read)
+# Student.all.each do |student|
+#   puts "#{student.id}"
+#   puts "#{student.name}"
+#   puts "#{student.image}"
 # end
 
+
+  # student_html = html.css("li.home-blog-post")
+
+  # img = student_html.css("div.blog-thumb a img")[1]['src']
+
+  # name = student_html.css("div.big-comment h3 a")[1].text
